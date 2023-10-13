673957608f471ade36c0c46660e20b052a975762
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-10-13 10:09:55 +0400

    создание функции

--- sandbox/src/main/java/myCours/MyFirstProgram.java
+++ sandbox/src/main/java/myCours/MyFirstProgram.java
@@ -3,6 +3,9 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
+        hello();
+    }
+    public static void hello(){ //Создание функции
         System.out.println("Hello world");
     }
 }
