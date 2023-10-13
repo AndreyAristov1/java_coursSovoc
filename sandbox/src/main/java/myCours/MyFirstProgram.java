1ad897c6277ac0248f3082e2022da4871f5e930d
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-10-13 10:48:26 +0400

    Добавлена новая функция

--- sandbox/src/main/java/myCours/MyFirstProgram.java
+++ sandbox/src/main/java/myCours/MyFirstProgram.java
@@ -6,9 +6,25 @@ public class MyFirstProgram {
         hello(" world");
         hello(" User");
         hello(" Andrey");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со сторонами " + len + "="+ area(len));
+
+        double a = 4;
+        double b = 6;
+
+        System.out.println("Площадь прямоугольника со сторонами " + a +" и " + b + "=" + area(a, b));
     }
     public static void hello(String sombody){ //Создание функции
 
         System.out.println("Hello" + sombody + "!");
+    }
+
+    public static double area(double l){ //Функция вычисления площади квадрата
+       return l * l;
+    }
+
+    public static double area(double a, double b){//Функция вычисления площади прямоугольника
+        return a*b;
     }
 }
