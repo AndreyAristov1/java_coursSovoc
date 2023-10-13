e130fdcd59cc8db264ccb80d506da4a888a00a0b
AndreyAristov1 <93186946+AndreyAristov1@users.noreply.github.com>
2023-10-13 10:19:25 +0400

    Добавление новых переменных

--- sandbox/src/main/java/myCours/MyFirstProgram.java
+++ sandbox/src/main/java/myCours/MyFirstProgram.java
@@ -4,6 +4,8 @@ public class MyFirstProgram {
     public static void main(String[] args) {
 
         hello(" world");
+        hello(" User");
+        hello(" Andrey");
     }
     public static void hello(String sombody){ //Создание функции
 
